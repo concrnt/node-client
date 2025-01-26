3def14a335c244bc85c5ed298852b7fcc6e62a67
totegamma <github@gammalab.net>
2025-01-26 19:27:10 +0900

    add swr handling

--- src/api.ts
+++ src/api.ts
@@ -25,8 +25,13 @@ class InvalidKeyError extends Error {
 export interface ApiResponse<T> {
     content: T
     status: 'ok' | 'error'
+    error: string
 }
 
+export interface FetchOptions<T> {
+    cache?: 'force-cache' | 'no-cache' | 'swr'
+    expressGetter?: (data: T) => void
+}
 
 export class Api {
 
@@ -40,42 +45,58 @@ export class Api {
     }
 
     // GET:/api/v1/message/:id
-    async getMessage<T>(id: string, host: string = ''): Promise<Message<T> | null> {
+    async getMessage<T>(id: string, host: string = '', opts?: FetchOptions<Message<T>>): Promise<Message<T> | null> {
 
         const cacheKey = `message:${id}`
-        const cached = await this.cache.get<Message<T>>(cacheKey)
-        if (cached) {
+
+        let cached: Message<T> | null = null
+        if (opts?.cache !== 'no-cache') {
+            cached = await this.cache.get<Message<T>>(cacheKey)
+            if (cached) {
+                opts?.expressGetter?.(cached)
+                if (opts?.cache !== 'swr' && !opts?.expressGetter) return cached
+            }
+        }
+        if (opts?.cache === 'force-cache') return null
+
+        const fetchNetwork = async (): Promise<Message<T> | null> => {
+            const authHeaders = await this.authProvider.getHeaders(host)
+
+            const requestOptions = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...authHeaders
+                }
+            }
+
+            const messageHost = host || this.host
+            const req = fetch(`https://${messageHost}/${apiPath}/message/${id}`, requestOptions).then(async (res) => {
+
+                if (!res.ok) {
+                    if (res.status === 404) return null 
+                    return await Promise.reject(new Error(`fetch failed on transport: ${res.status} ${await res.text()}`))
+                }
+
+                const data: ApiResponse<Message<T>> = await res.json()
+                if (data.status != 'ok') {
+                    return await Promise.reject(new Error(`getMessage failed on application: ${data.error}`))
+                }
+                
+                opts?.expressGetter?.(data.content)
+                return data.content
+            })
+
+            this.cache.set(cacheKey, req)
+            return req
+        }
+
+        if (opts?.cache === 'swr' && cached) {
+            fetchNetwork()
             return cached
         }
 
-        const authHeaders = await this.authProvider.getHeaders(host)
-
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                ...authHeaders
-            }
-        }
-
-        const messageHost = host || this.host
-        const req = fetch(`https://${messageHost}/${apiPath}/message/${id}`, requestOptions).then(async (res) => {
-
-            if (!res.ok) {
-                if (res.status === 404) return null 
-                return await Promise.reject(new Error(`fetch failed on transport: ${res.status} ${await res.text()}`))
-            }
-
-            const data = await res.json()
-            if (data.status != 'ok') {
-                return await Promise.reject(new Error(`getMessage failed on application: ${data.error}`))
-            }
-
-            return data.content
-        })
-
-        this.cache.set(cacheKey, req)
-        return req
+        return await fetchNetwork()
     }
 
 
